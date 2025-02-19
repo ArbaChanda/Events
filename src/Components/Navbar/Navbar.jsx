a363dbee0594431114198e24ccbe124aa535a433
Arba Chanda <arbachanda31@gmail.com>
2025-02-19 14:34:54 +0530

    Setting up the project

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+import Navlist from './Navlist/Navlist';
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+      <h1>Salon Madrid</h1>
+      <Navlist />
+    </div>
+  )
+}
+
+export default Navbar
