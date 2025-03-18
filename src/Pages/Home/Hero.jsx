66bff88227667b45439c95b6fa89260d13e97ad7
Arba Chanda <arbachanda31@gmail.com>
2025-03-18 18:01:58 +0530

    Small hero section change

--- src/Pages/Home/Hero.jsx
+++ src/Pages/Home/Hero.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CTA_btn from '../../Components/CTA_btn/CTA_btn'
+import heroImg from '../../assets/images/home/heroDesktop.png' 
 import './hero.css'
 
 const Hero = () => {
   return (
     <div className='hero-section'>
-        <img className='mobile-version-img' src="src\assets\images\home\heroDesktop.png" alt="image" />
+        <img className='mobile-version-img' src={heroImg} alt="image" />
+        {/* <img className='mobile-version-img' src="src\assets\images\home\heroDesktop.png" alt="image" /> */}
         <div className="discription">
           <h2>Unforgettable Events, All in One Place</h2>
           <h3>Weddings, Birthday parties, Gatherings and more...</h3>
